ed026b585b17223e327e405e31957af9c93e5727
Paul Olteanu <p.a.olteanu@gmail.com>
2024-09-18 13:35:27 -0400

    idk

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,13 @@ uniffi::setup_scaffolding!();
 
 use std::{
     net::SocketAddr,
-    sync::Arc,
     time::{Duration, Instant},
 };
 
-use quinn::{crypto::rustls::QuicClientConfig, ClientConfig, TransportConfig};
-use rustls::pki_types::{CertificateDer, ServerName, UnixTime};
+use tokio::net::UdpSocket;
 
 #[uniffi::export]
-pub fn run(url: String, bandwidth: f64, duration: u64) {
+pub fn run(url: String, bandwidth: f64, duration: u64, buffer_size: u64) {
     let rt = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
@@ -18,19 +16,12 @@ pub fn run(url: String, bandwidth: f64, duration: u64) {
 
     rt.block_on(async move {
         let addr = SocketAddr::from(([0, 0, 0, 0], 0));
-        let endpoint = quinn::Endpoint::client(addr).unwrap();
+        let socket = UdpSocket::bind(addr).await.unwrap();
 
-        let addr = tokio::net::lookup_host(url).await.unwrap().next().unwrap();
-        let connection = endpoint
-            .connect_with(configure_client(), addr, "speedtest")
-            .unwrap()
-            .await
-            .unwrap();
+        let server_addr = tokio::net::lookup_host(url).await.unwrap().next().unwrap();
 
-        let (mut send_stream, _recv_stream) = connection.open_bi().await.unwrap();
-
-        let bytes_per_sec = (1024 * 1024) as f64 * bandwidth / 8.0;
-        let sends_per_sec = bytes_per_sec / 2048.0;
+        let bytes_per_sec = (bandwidth / 8.0) * (1024.0 * 1024.0);
+        let sends_per_sec = bytes_per_sec / buffer_size as f64;
 
         println!("starting speed test");
         let start = Instant::now();
@@ -40,84 +31,14 @@ pub fn run(url: String, bandwidth: f64, duration: u64) {
 
         while start.elapsed() <= Duration::from_secs(duration) {
             interval.tick().await;
-            let data = vec![0; 2048];
-            send_stream.write_all(&data).await.unwrap();
+            let data = vec![0; buffer_size as usize];
+
+            socket.send_to(&data, &server_addr).await.unwrap();
         }
 
-        send_stream.finish().unwrap();
-        send_stream.stopped().await.unwrap();
+        let data = [1; 32];
+        socket.send_to(&data, server_addr).await.unwrap();
+
         println!("finished speed test");
     });
-}
-
-pub fn configure_client() -> ClientConfig {
-    let mut client_config = ClientConfig::new(Arc::new(
-        QuicClientConfig::try_from(
-            rustls::ClientConfig::builder()
-                .dangerous()
-                .with_custom_certificate_verifier(SkipServerVerification::new())
-                .with_no_client_auth(),
-        )
-        .unwrap(),
-    ));
-
-    let mut transport_config = TransportConfig::default();
-    transport_config.max_idle_timeout(None); // TODO: IS THIS SUS?
-    client_config.transport_config(Arc::new(transport_config));
-
-    client_config
-}
-
-#[derive(Debug)]
-struct SkipServerVerification(Arc<rustls::crypto::CryptoProvider>);
-
-impl SkipServerVerification {
-    fn new() -> Arc<Self> {
-        Arc::new(Self(Arc::new(rustls::crypto::ring::default_provider())))
-    }
-}
-
-impl rustls::client::danger::ServerCertVerifier for SkipServerVerification {
-    fn verify_server_cert(
-        &self,
-        _end_entity: &CertificateDer<'_>,
-        _intermediates: &[CertificateDer<'_>],
-        _server_name: &ServerName<'_>,
-        _ocsp: &[u8],
-        _now: UnixTime,
-    ) -> Result<rustls::client::danger::ServerCertVerified, rustls::Error> {
-        Ok(rustls::client::danger::ServerCertVerified::assertion())
-    }
-
-    fn verify_tls12_signature(
-        &self,
-        message: &[u8],
-        cert: &CertificateDer<'_>,
-        dss: &rustls::DigitallySignedStruct,
-    ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
-        rustls::crypto::verify_tls12_signature(
-            message,
-            cert,
-            dss,
-            &self.0.signature_verification_algorithms,
-        )
-    }
-
-    fn verify_tls13_signature(
-        &self,
-        message: &[u8],
-        cert: &CertificateDer<'_>,
-        dss: &rustls::DigitallySignedStruct,
-    ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
-        rustls::crypto::verify_tls13_signature(
-            message,
-            cert,
-            dss,
-            &self.0.signature_verification_algorithms,
-        )
-    }
-
-    fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
-        self.0.signature_verification_algorithms.supported_schemes()
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,48 @@
-use std::{net::SocketAddr, sync::Arc, time::Instant};
+use std::{collections::HashMap, net::SocketAddr, time::Instant};
 
-use quinn::{Endpoint, ServerConfig};
-use rustls::pki_types::{CertificateDer, PrivatePkcs8KeyDer};
+use tokio::net::UdpSocket;
+
+struct Data {
+    start: Instant,
+    amount: usize,
+}
 
 #[tokio::main]
 async fn main() {
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
-    let (config, _) = configure_server();
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
+    let socket = UdpSocket::bind(addr).await.unwrap();
+    let mut buf = [0; 16384];
 
-    let endpoint = Endpoint::server(config, addr).unwrap();
+    let mut in_progress: HashMap<SocketAddr, Data> = HashMap::new();
 
     loop {
-        while let Some(incoming) = endpoint.accept().await {
-            tokio::spawn(async move {
-                let connection = incoming.accept().unwrap().await.unwrap();
-                println!("got connection");
-
-                let (send_stream, mut recv_stream) = connection.accept_bi().await.unwrap();
-
-                let start = Instant::now();
-                let mut total_sent = 0;
-                let mut buf = vec![9; 4096];
-                while let Ok(Some(n)) = recv_stream.read(&mut buf).await {
-                    println!("received data chunk len={}", n);
-                    total_sent += n;
-                }
-
-                println!("connection finished");
-                println!("{} bytes sent in {:?}", total_sent, start.elapsed());
-            });
+        let (n, addr) = socket.recv_from(&mut buf).await.unwrap();
+        if n == 0 {
+            continue;
         }
+
+        if buf[0] == 1 {
+            if let Some(data) = in_progress.remove(&addr) {
+                let amount = data.amount + n;
+                let duration = data.start.elapsed();
+                let throughput = (((amount * 8) / (1024 * 1024)) as f64) / duration.as_secs_f64();
+                println!(
+                    "received {} bytes over {:?} from {}. throughput = {} mbps",
+                    amount, duration, addr, throughput
+                );
+            }
+            continue;
+        }
+
+        let entry = in_progress.entry(addr).or_insert_with(|| {
+            println!("adding new client {}", addr);
+            Data {
+                start: Instant::now(),
+                amount: 0,
+            }
+        });
+        entry.amount += n;
+
+        // println!("received {} bytes from {}", x, y);
     }
-}
-
-pub(crate) fn configure_server() -> (ServerConfig, CertificateDer<'static>) {
-    let cert =
-        rcgen::generate_simple_self_signed(vec!["localhost".into(), "hostname".into()]).unwrap();
-    let cert_der = CertificateDer::from(cert.cert);
-    let priv_key = PrivatePkcs8KeyDer::from(cert.key_pair.serialize_der());
-
-    let mut server_config =
-        ServerConfig::with_single_cert(vec![cert_der.clone()], priv_key.into()).unwrap();
-    let transport_config = Arc::get_mut(&mut server_config.transport).unwrap();
-    transport_config.max_concurrent_uni_streams(0_u8.into());
-    transport_config.max_idle_timeout(None); // TODO: IS THIS SUS?
-
-    (server_config, cert_der)
 }
