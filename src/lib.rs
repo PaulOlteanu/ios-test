1f02c8da0cea3836c1e54a7dd88c590ddb147947
Paul Olteanu <p.a.olteanu@gmail.com>
2024-09-19 14:52:23 -0400

    something

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,280 @@
 uniffi::setup_scaffolding!();
 
 use std::{
+    collections::HashMap,
     net::SocketAddr,
+    sync::{Arc, Mutex},
     time::{Duration, Instant},
 };
 
+use serde::{Deserialize, Serialize};
+use str0m::{
+    change::SdpAnswer,
+    channel::{ChannelConfig, Reliability},
+    net::{Protocol, Receive},
+    Candidate, Event, IceConnectionState, Input, Output, Rtc,
+};
+use stunclient::StunClient;
 use tokio::net::UdpSocket;
 
+#[derive(Serialize, Deserialize)]
+struct Candidates {
+    candidates: Vec<Candidate>,
+}
+
 #[uniffi::export]
-pub fn run(url: String, bandwidth: f64, duration: u64, buffer_size: u64) {
+pub fn run(host: String, port: u16, bandwidth: f64, duration: u64, buffer_size: u64) {
     let rt = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
         .unwrap();
 
     rt.block_on(async move {
+        tracing_subscriber::fmt::init();
+
+        let mut rtc = Rtc::new();
+        let server_url = format!("http://{host}:{port}");
+        let client = reqwest::Client::new();
+
         let addr = SocketAddr::from(([0, 0, 0, 0], 0));
-        let socket = UdpSocket::bind(addr).await.unwrap();
+        let host_socket = UdpSocket::bind(addr).await.unwrap();
+        let host_socket = Arc::new(host_socket);
+        let host_addr =
+            SocketAddr::from(([127, 0, 0, 1], host_socket.local_addr().unwrap().port()));
 
-        let server_addr = tokio::net::lookup_host(url).await.unwrap().next().unwrap();
-        println!("sending to {}", server_addr);
+        let addr = SocketAddr::from(([0, 0, 0, 0], 0));
+        let srflx_socket = UdpSocket::bind(addr).await.unwrap();
+        let srflx_socket = Arc::new(srflx_socket);
+        let srflx_addr =
+            SocketAddr::from(([127, 0, 0, 1], srflx_socket.local_addr().unwrap().port()));
 
-        socket.connect(server_addr).await.unwrap();
+        println!("getting srflx addr");
+
+        let stun_addr = "stun.l.google.com:19302";
+        let stun_addr = tokio::net::lookup_host(stun_addr)
+            .await
+            .unwrap()
+            .next()
+            .unwrap();
+
+        let stun_client = StunClient::new(stun_addr);
+        let srflx = stun_client
+            .query_external_address_async(&srflx_socket)
+            .await
+            .unwrap();
+
+        println!("got srflx addr {}", stun_addr);
+
+        let mut candidates = Vec::new();
+        let mut sockets = HashMap::new();
+
+        let candidate = Candidate::host(host_addr, "udp").unwrap();
+        rtc.add_local_candidate(candidate.clone());
+        candidates.push(candidate);
+        sockets.insert(host_addr, host_socket.clone());
+
+        let base = SocketAddr::from(([0, 0, 0, 0], srflx_socket.local_addr().unwrap().port()));
+        let candidate = Candidate::server_reflexive(srflx, base, "udp").unwrap();
+        rtc.add_local_candidate(candidate.clone());
+        candidates.push(candidate);
+        sockets.insert(base, srflx_socket.clone());
+
+        let mut change = rtc.sdp_api();
+        let channel_config = ChannelConfig {
+            label: "jklfds".to_owned(),
+            ordered: false,
+            reliability: Reliability::MaxRetransmits { retransmits: 0 },
+            negotiated: None,
+            protocol: "jklfds".to_owned(),
+        };
+
+        change.add_channel_with_config(channel_config);
+
+        let (offer, pending) = change.apply().unwrap();
+
+        let reply = client
+            .post(format!("{server_url}/offer"))
+            .body(offer.to_sdp_string())
+            .send()
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap();
+
+        let answer = SdpAnswer::from_sdp_string(&reply).unwrap();
+        rtc.sdp_api().accept_answer(pending, answer).unwrap();
+
+        let candidates = Candidates { candidates };
+        let candidates: Candidates = client
+            .post(format!("{server_url}/candidate"))
+            .json(&candidates)
+            .send()
+            .await
+            .unwrap()
+            .json()
+            .await
+            .unwrap();
+
+        for candidate in candidates.candidates {
+            rtc.add_remote_candidate(candidate);
+        }
+
+        let data_channel_id = Mutex::new(None);
 
         let bytes_per_sec = (bandwidth / 8.0) * (1024.0 * 1024.0);
         let sends_per_sec = bytes_per_sec / buffer_size as f64;
 
         println!("starting speed test");
 
-        let start = Instant::now();
-
         let mut interval =
             tokio::time::interval(Duration::try_from_secs_f64(1.0 / sends_per_sec).unwrap());
 
-        while start.elapsed() <= Duration::from_secs(duration) {
-            interval.tick().await;
+        let end = tokio::time::sleep(Duration::from_secs(duration));
+        tokio::pin!(end);
 
-            let data = vec![0; buffer_size as usize];
+        let mut done = false;
 
-            let n = socket.send(&data).await.unwrap();
+        loop {
+            let timeout = match rtc.poll_output().unwrap() {
+                // Stop polling when we get the timeout.
+                Output::Timeout(v) => v,
+
+                // Transmit this data to the remote peer. Typically via
+                // a UDP socket. The destination IP comes from the ICE
+                // agent. It might change during the session.
+                Output::Transmit(v) => {
+                    if let Some(socket) = sockets.get(&v.source) {
+                        socket.send_to(&v.contents, v.destination).await.unwrap();
+                    }
+                    continue;
+                }
+
+                // Events are mainly incoming media data from the remote
+                // peer, but also data channel data and statistics.
+                Output::Event(v) => {
+                    // Abort if we disconnect.
+                    if v == Event::IceConnectionStateChange(IceConnectionState::Disconnected) {
+                        return;
+                    }
+
+                    match v {
+                        Event::Connected => {
+                            println!("connected");
+                        }
+
+                        Event::IceConnectionStateChange(state) => {
+                            println!("Ice connection state change {:?}", state);
+                        }
+
+                        Event::ChannelOpen(id, name) => {
+                            *data_channel_id.lock().unwrap() = Some(id);
+                            println!("data channel opened {:?} {name}", id);
+                        }
+
+                        Event::ChannelData(data) => {
+                            println!("data channel data {:?}", data);
+                        }
+
+                        Event::ChannelClose(id) => {
+                            println!("data channel closed {:?}", id);
+                        }
+                        _ => {}
+                    }
+
+                    continue;
+                }
+            };
+
+            if done {
+                break;
+            }
+
+            let mut buf1 = vec![0; 2048];
+            let mut buf2 = vec![0; 2048];
+
+            let input = tokio::select! {
+                input = host_socket.recv_from(&mut buf1) => {
+                    let (n, source) = input.unwrap();
+                    buf1.truncate(n);
+
+                    let destination = host_addr;
+
+                    Input::Receive(
+                        Instant::now(),
+                        Receive {
+                            proto: Protocol::Udp,
+                            source,
+                            destination,
+                            contents: buf1.as_slice().try_into().unwrap(),
+                        }
+                    )
+                }
+
+                input = srflx_socket.recv_from(&mut buf2) => {
+                    let (n, source) = input.unwrap();
+                    buf2.truncate(n);
+
+                    let destination = srflx_addr;
+
+                    Input::Receive(
+                        Instant::now(),
+                        Receive {
+                            proto: Protocol::Udp,
+                            source,
+                            destination,
+                            contents: buf2.as_slice().try_into().unwrap(),
+                        }
+                    )
+                }
+
+                _ = interval.tick() => {
+                    if let Some(id) = *data_channel_id.lock().unwrap() {
+                        let data = vec![0; buffer_size as usize];
+                        rtc.channel(id).unwrap().write(false, &data).unwrap();
+                    }
+
+                    continue;
+                }
+
+                _ = &mut end => {
+                    let data = vec![1; buffer_size as usize];
+                    if let Some(id) = *data_channel_id.lock().unwrap() {
+                        rtc.channel(id).unwrap().write(false, &data).unwrap();
+                    }
+
+                    println!("finished speed test");
+                    done = true;
+                    continue;
+                }
+
+                _ = tokio::time::sleep_until(timeout.into()) => {
+                    Input::Timeout(Instant::now())
+                }
+            };
+
+            // Input is either a Timeout or Receive of data. Both drive the state forward.
+            rtc.handle_input(input).unwrap();
         }
-
-        let data = vec![1; buffer_size as usize];
-        let n = socket.send(&data).await.unwrap();
-
-        println!("finished speed test");
     });
+}
+
+use std::net::IpAddr;
+use systemstat::{Platform, System};
+
+pub fn select_host_address() -> IpAddr {
+    let system = System::new();
+    let networks = system.networks().unwrap();
+
+    for net in networks.values() {
+        for n in &net.addrs {
+            if let systemstat::IpAddr::V4(v) = n.addr {
+                if !v.is_loopback() && !v.is_link_local() && !v.is_broadcast() {
+                    return IpAddr::V4(v);
+                }
+            }
+        }
+    }
+
+    panic!("Found no usable network interface");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,219 @@
-use std::net::{SocketAddr, UdpSocket};
-use std::{collections::HashMap, time::Instant};
+use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+use std::time::Instant;
+
+use axum::extract::State;
+use axum::response::IntoResponse;
+use axum::routing::{get, post};
+use axum::{debug_handler, Json, Router};
+use serde::{Deserialize, Serialize};
+use str0m::change::SdpOffer;
+use str0m::net::{Protocol, Receive};
+use str0m::{Candidate, Event, IceConnectionState, Input, Output, Rtc};
+use tokio::net::{TcpSocket, UdpSocket};
+use tokio::sync::mpsc;
+use tower_http::trace::{DefaultMakeSpan, TraceLayer};
+use tracing::Level;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Candidates {
+    candidates: Vec<Candidate>,
+}
 
 struct Data {
     start: Instant,
     amount: usize,
 }
 
-fn main() {
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
-    let socket = UdpSocket::bind(addr).unwrap();
-    let mut buf = vec![0; 16384];
+struct AppState {
+    rtc: Rtc,
+    candidates: Vec<Candidate>,
+    ready_send: mpsc::UnboundedSender<()>,
+}
 
-    let mut in_progress: HashMap<SocketAddr, Data> = HashMap::new();
+type WrappedState = Arc<Mutex<Option<AppState>>>;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let socket = UdpSocket::bind("0.0.0.0:8080").await.unwrap();
+
+    let mut rtc = Rtc::new();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let candidate = Candidate::host(addr, "udp").unwrap();
+
+    rtc.add_local_candidate(candidate.clone());
+
+    let (ready_send, mut ready_recv) = mpsc::unbounded_channel();
+
+    let candidates = vec![candidate];
+    let state = AppState {
+        rtc,
+        candidates,
+        ready_send,
+    };
+    let state = Arc::new(Mutex::new(Some(state)));
+    let app = Router::new()
+        .route("/offer", post(offer_handler))
+        .route("/candidate", post(candidate_handler))
+        .with_state(state.clone());
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    tokio::spawn(async move {
+        axum::serve(listener, app).await.unwrap();
+    });
+
+    ready_recv.recv().await.unwrap();
+    let state = state.lock().unwrap().take().unwrap();
+
+    let AppState { mut rtc, .. } = state;
+
+    let mut received = 0;
+    let mut start = None;
 
     loop {
-        let (n, addr) = socket.recv_from(&mut buf).unwrap();
-        if n == 0 {
-            continue;
-        }
+        let timeout = match rtc.poll_output().unwrap() {
+            // Stop polling when we get the timeout.
+            Output::Timeout(v) => v,
 
-        if buf[0] == 1 {
-            if let Some(data) = in_progress.remove(&addr) {
-                let amount = data.amount + n;
-                let duration = data.start.elapsed();
-                let throughput = (((amount * 8) / (1024 * 1024)) as f64) / duration.as_secs_f64();
-                println!(
-                    "received {} bytes over {:?} from {}. throughput = {} mbps",
-                    amount, duration, addr, throughput
-                );
+            // Transmit this data to the remote peer. Typically via
+            // a UDP socket. The destination IP comes from the ICE
+            // agent. It might change during the session.
+            Output::Transmit(v) => {
+                socket.send_to(&v.contents, v.destination).await.unwrap();
+                continue;
             }
-            continue;
-        }
 
-        let entry = in_progress.entry(addr).or_insert_with(|| {
-            println!("adding new client {}", addr);
-            Data {
-                start: Instant::now(),
-                amount: 0,
+            // Events are mainly incoming media data from the remote
+            // peer, but also data channel data and statistics.
+            Output::Event(v) => {
+                // Abort if we disconnect.
+                if v == Event::IceConnectionStateChange(IceConnectionState::Disconnected) {
+                    return;
+                }
+
+                match v {
+                    Event::Connected => {
+                        println!("connected");
+                    }
+
+                    Event::IceConnectionStateChange(state) => {
+                        println!("Ice connection state change {:?}", state);
+                    }
+
+                    Event::ChannelOpen(id, name) => {
+                        println!("data channel opened {:?} {name}", id);
+                    }
+
+                    Event::ChannelData(data) => {
+                        if start.is_none() {
+                            start = Some(Instant::now());
+                        }
+
+                        received += data.data.len();
+
+                        if data.data.first() == Some(&1) {
+                            let elapsed = start.unwrap().elapsed();
+
+                            let throughput =
+                                (((received * 8) / (1024 * 1024)) as f64) / elapsed.as_secs_f64();
+                            println!(
+                                "received {} bytes over {:?}. throughput = {} mbps",
+                                received, elapsed, throughput
+                            );
+                            break;
+                        }
+                    }
+
+                    Event::ChannelClose(id) => {
+                        println!("data channel closed {:?}", id);
+                    }
+                    _ => {}
+                }
+
+                continue;
             }
-        });
-        entry.amount += n;
+        };
+
+        let mut buf = vec![0; 4096];
+
+        let input = tokio::select! {
+            input = socket.recv_from(&mut buf) => {
+                let (n, source) = input.unwrap();
+                buf.truncate(n);
+
+                let destination = addr;
+
+                Input::Receive(
+                    Instant::now(),
+                    Receive {
+                        proto: Protocol::Udp,
+                        source,
+                        destination,
+                        contents: buf.as_slice().try_into().unwrap(),
+                    }
+                )
+            }
+
+            _ = tokio::time::sleep_until(timeout.into()) => {
+                Input::Timeout(Instant::now())
+            }
+        };
+
+        // Input is either a Timeout or Receive of data. Both drive the state forward.
+        rtc.handle_input(input).unwrap();
     }
+}
+
+#[debug_handler]
+async fn offer_handler(State(state): State<WrappedState>, body: String) -> impl IntoResponse {
+    let offer = SdpOffer::from_sdp_string(&body).unwrap();
+
+    let mut state = state.lock().unwrap();
+    let state = state.as_mut().unwrap();
+    let answer = state.rtc.sdp_api().accept_offer(offer).unwrap();
+
+    answer.to_sdp_string()
+}
+
+#[debug_handler]
+async fn candidate_handler(
+    State(state): State<WrappedState>,
+    Json(candidates): Json<Candidates>,
+) -> impl IntoResponse {
+    let mut state = state.lock().unwrap();
+    let state = state.as_mut().unwrap();
+    for candidate in candidates.candidates {
+        state.rtc.add_remote_candidate(candidate);
+    }
+
+    state.ready_send.send(()).unwrap();
+
+    let candidates = Candidates {
+        candidates: state.candidates.clone(),
+    };
+    Json(candidates)
+}
+
+use std::net::IpAddr;
+use systemstat::{Platform, System};
+
+pub fn select_host_address() -> IpAddr {
+    let system = System::new();
+    let networks = system.networks().unwrap();
+
+    for net in networks.values() {
+        for n in &net.addrs {
+            if let systemstat::IpAddr::V4(v) = n.addr {
+                if !v.is_loopback() && !v.is_link_local() && !v.is_broadcast() {
+                    return IpAddr::V4(v);
+                }
+            }
+        }
+    }
+
+    panic!("Found no usable network interface");
 }
