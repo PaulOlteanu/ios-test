24f2c13538b71a00e768ddbf323db53fc28afecf
Paul Olteanu <p.a.olteanu@gmail.com>
2024-09-20 17:31:58 -0400

    idk

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::net::SocketAddr;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::time::Instant;
 
 use axum::extract::State;
@@ -11,32 +11,25 @@ use str0m::change::SdpOffer;
 use str0m::net::{Protocol, Receive};
 use str0m::{Candidate, Event, IceConnectionState, Input, Output, Rtc};
 use tokio::net::{TcpSocket, UdpSocket};
-use tokio::sync::mpsc;
-use tracing::Level;
+use tokio::sync::{mpsc, Mutex};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Candidates {
-    candidates: Vec<Candidate>,
-}
-
-struct Data {
-    start: Instant,
-    amount: usize,
+    candidates: Vec<String>,
 }
 
 struct AppState {
     rtc: Rtc,
-    candidates: Vec<Candidate>,
     ready_send: mpsc::UnboundedSender<()>,
 }
 
-type WrappedState = Arc<Mutex<Option<AppState>>>;
+type WrappedState = Arc<Mutex<AppState>>;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let socket = UdpSocket::bind("0.0.0.0:8080").await.unwrap();
+    let socket = UdpSocket::bind("0.0.0.0:8000").await.unwrap();
 
     let mut rtc = Rtc::new();
 
@@ -48,93 +41,90 @@ async fn main() {
 
     let (ready_send, mut ready_recv) = mpsc::unbounded_channel();
 
-    let candidates = vec![candidate];
-    let state = AppState {
-        rtc,
-        candidates,
-        ready_send,
-    };
-    let state = Arc::new(Mutex::new(Some(state)));
+    let state = AppState { rtc, ready_send };
+    let state = Arc::new(Mutex::new(state));
+
     let app = Router::new()
         .route("/offer", post(offer_handler))
         .route("/candidate", post(candidate_handler))
         .with_state(state.clone());
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
     tokio::spawn(async move {
         axum::serve(listener, app).await.unwrap();
     });
 
     ready_recv.recv().await.unwrap();
-    let state = state.lock().unwrap().take().unwrap();
-
-    let AppState { mut rtc, .. } = state;
 
     let mut received = 0;
     let mut start = None;
 
     loop {
-        let timeout = match rtc.poll_output().unwrap() {
-            // Stop polling when we get the timeout.
-            Output::Timeout(v) => v,
+        let timeout = {
+            let mut state = state.lock().await;
 
-            // Transmit this data to the remote peer. Typically via
-            // a UDP socket. The destination IP comes from the ICE
-            // agent. It might change during the session.
-            Output::Transmit(v) => {
-                socket.send_to(&v.contents, v.destination).await.unwrap();
-                continue;
-            }
+            match state.rtc.poll_output().unwrap() {
+                // Stop polling when we get the timeout.
+                Output::Timeout(v) => v,
 
-            // Events are mainly incoming media data from the remote
-            // peer, but also data channel data and statistics.
-            Output::Event(v) => {
-                // Abort if we disconnect.
-                if v == Event::IceConnectionStateChange(IceConnectionState::Disconnected) {
-                    return;
+                // Transmit this data to the remote peer. Typically via
+                // a UDP socket. The destination IP comes from the ICE
+                // agent. It might change during the session.
+                Output::Transmit(v) => {
+                    socket.send_to(&v.contents, v.destination).await.unwrap();
+                    continue;
                 }
 
-                match v {
-                    Event::Connected => {
-                        println!("connected");
+                // Events are mainly incoming media data from the remote
+                // peer, but also data channel data and statistics.
+                Output::Event(v) => {
+                    // Abort if we disconnect.
+                    if v == Event::IceConnectionStateChange(IceConnectionState::Disconnected) {
+                        return;
                     }
 
-                    Event::IceConnectionStateChange(state) => {
-                        println!("Ice connection state change {:?}", state);
-                    }
-
-                    Event::ChannelOpen(id, name) => {
-                        println!("data channel opened {:?} {name}", id);
-                    }
-
-                    Event::ChannelData(data) => {
-                        if start.is_none() {
-                            start = Some(Instant::now());
+                    match v {
+                        Event::Connected => {
+                            println!("connected");
                         }
 
-                        received += data.data.len();
-
-                        if data.data.first() == Some(&1) {
-                            let elapsed = start.unwrap().elapsed();
-
-                            let throughput =
-                                (((received * 8) / (1024 * 1024)) as f64) / elapsed.as_secs_f64();
-                            println!(
-                                "received {} bytes over {:?}. throughput = {} mbps",
-                                received, elapsed, throughput
-                            );
-                            break;
+                        Event::IceConnectionStateChange(state) => {
+                            println!("Ice connection state change {:?}", state);
                         }
+
+                        Event::ChannelOpen(id, name) => {
+                            println!("data channel opened {:?} {name}", id);
+                        }
+
+                        Event::ChannelData(data) => {
+                            if start.is_none() {
+                                start = Some(Instant::now());
+                            }
+
+                            received += data.data.len();
+
+                            if data.data.first() == Some(&1) {
+                                let elapsed = start.unwrap().elapsed();
+
+                                let throughput = (((received * 8) / (1024 * 1024)) as f64)
+                                    / elapsed.as_secs_f64();
+                                println!(
+                                    "received {} bytes over {:?}. throughput = {} mbps",
+                                    received, elapsed, throughput
+                                );
+                                break;
+                            }
+                        }
+
+                        Event::ChannelClose(id) => {
+                            println!("data channel closed {:?}", id);
+                        }
+                        _ => {}
                     }
 
-                    Event::ChannelClose(id) => {
-                        println!("data channel closed {:?}", id);
-                    }
-                    _ => {}
+                    continue;
                 }
-
-                continue;
             }
         };
 
@@ -163,8 +153,9 @@ async fn main() {
             }
         };
 
+        let mut state = state.lock().await;
         // Input is either a Timeout or Receive of data. Both drive the state forward.
-        rtc.handle_input(input).unwrap();
+        state.rtc.handle_input(input).unwrap();
     }
 }
 
@@ -172,28 +163,18 @@ async fn main() {
 async fn offer_handler(State(state): State<WrappedState>, body: String) -> impl IntoResponse {
     let offer = SdpOffer::from_sdp_string(&body).unwrap();
 
-    let mut state = state.lock().unwrap();
-    let state = state.as_mut().unwrap();
+    let mut state = state.lock().await;
     let answer = state.rtc.sdp_api().accept_offer(offer).unwrap();
+
+    state.ready_send.send(()).unwrap();
 
     answer.to_sdp_string()
 }
 
 #[debug_handler]
-async fn candidate_handler(
-    State(state): State<WrappedState>,
-    Json(candidates): Json<Candidates>,
-) -> impl IntoResponse {
-    let mut state = state.lock().unwrap();
-    let state = state.as_mut().unwrap();
-    for candidate in candidates.candidates {
-        state.rtc.add_remote_candidate(candidate);
-    }
+async fn candidate_handler(State(state): State<WrappedState>, body: String) -> impl IntoResponse {
+    let mut state = state.lock().await;
+    let candidate = Candidate::from_sdp_string(&body).unwrap();
 
-    state.ready_send.send(()).unwrap();
-
-    let candidates = Candidates {
-        candidates: state.candidates.clone(),
-    };
-    Json(candidates)
+    state.rtc.add_remote_candidate(candidate);
 }
