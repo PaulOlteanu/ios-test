5ee98f83aedc9a4fd932c87f52acfbaeb662fb57
Paul Olteanu <p.a.olteanu@gmail.com>
2024-09-24 14:24:18 -0400

    lite

--- src/main.rs
+++ src/main.rs
@@ -63,10 +63,10 @@ async fn main() {
 
     // Prepare the configuration
     let config = RTCConfiguration {
-        ice_servers: vec![RTCIceServer {
-            urls: vec!["stun:stun.l.google.com:19302".to_owned()],
-            ..Default::default()
-        }],
+        // ice_servers: vec![RTCIceServer {
+        //     urls: vec!["stun:stun.l.google.com:19302".to_owned()],
+        //     ..Default::default()
+        // }],
         ..Default::default()
     };
 
